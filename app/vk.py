e46c3498e750f6968d069870ae76c8405301f92d
darkj0k <rybushkin09@bk.ru>
2025-01-08 17:22:22 +0400

    Fix: vk

--- app/vk.py
+++ app/vk.py
@@ -8,6 +8,7 @@ class VkDeleter:
         self.vk_session = vk_api.VkApi(
             token=token)
         self.vk = self.vk_session.get_api()
+        self.id_group = self.get_group()
         self.run()
 
     def run(self):
@@ -15,21 +16,22 @@ class VkDeleter:
             self.loop()
 
     def loop(self):
-        id_group = self.get_group()
         history_messages = self.vk.messages.getHistory(count=150, offset=0,
-                                                       peer_id=id_group)
+                                                       peer_id=self.id_group)
         for history_message in history_messages['items']:
             if "Для покупки любого из лотов" in history_message['text']:
-                self.delete_message(history_message['id'], id_group)
+                self.delete_message(message_id=history_message['id'], peer_id=self.id_group)
             elif "Вы успешно приобрели с аукциона предмет" in history_message['text']:
-                self.delete_message(history_message['id'], id_group)
+                self.delete_message(message_id=history_message['id'], peer_id=self.id_group)
 
     def get_group(self):
         list_conversation = self.vk.messages.getConversations(count=15)
+        logger.info("Получены все чаты")
         for conversation in list_conversation['items']:
             if conversation['conversation']['peer']['type'] == 'group':
                 group = self.vk.groups.getById(group_id=abs(conversation['conversation']['peer']['id']))
                 if group[0]['name'] == "Подземелье колодца":
+                    logger.info("Нашел группу")
                     return conversation['conversation']['peer']['id']
 
     def delete_message(self, message_id, peer_id):
